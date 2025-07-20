d080fd8ec068536375b371d81f38682a6677fffa
Vinicius Medeiros <vinicius.m3deiros@gmail.com>
2025-07-20 00:31:28 -0300

    Adiciona a configuração de exportação no next.config.ts, permitindo a exportação estática do projeto.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
   trailingSlash: true, // opcional: adiciona "/" ao final das URLs
   // Configuração de porta (alternativa)
   // experimental: {
